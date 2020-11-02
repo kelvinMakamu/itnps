b4e10b187a8d28e9d42837392a509da6559b8ddc
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-11-03 02:24:37 +0300

    Added user business logic and authentication/authorization logic

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,16 +1,27 @@
 const router   = require("express").Router();
-/**************
- CONTROLLERS
-*****************/
-const authCtrl = require('../controllers/auth.controller');
-
-/***********
- AUTH
-*****************/
-router.get('/',authCtrl.login,(req,res) =>{
-    res.json('Yellow');
-})
+/********
+ * 
+ * CTRLS
+ *
+**********/
+const authCtrl   = require('../controllers/auth.controller');
+const userCtrl   = require('../controllers/user.controller');
+const authorized = [ authCtrl.loginRequired, authCtrl.isValidToken ];
+/*****
+ * 
+ * AUTH
+ *
+********/
 router.post("/auth/login", authCtrl.login);
-router.post("/auth/register", authCtrl.registerUser);
+router.post("/auth/register",authCtrl.registerUser);
+
+/******
+ * 
+ * USERS
+ *
+********/
+router.get("/users",userCtrl.findUsers);
+router.get("/users/:id",userCtrl.findUserById);
+router.post("/users/userLevel",userCtrl.findUserByLevel);
 
 module.exports  = router;
