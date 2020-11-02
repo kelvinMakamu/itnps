2d08cce068df81b7c7b705c0cc1355ec8476ac99
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-11-02 15:09:33 +0300

    Adds database connection logic

---
+++ src/app/index.js
@@ -0,0 +1,25 @@
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const morgan      = require('morgan');
+const cors        = require('cors');
+const CONFIG      = require('../configs/config');
+const CONNECT     = require('../models/connect');
+
+const middlewares = [
+    morgan('dev'),
+    bodyParser.json(),
+    cors(CONFIG.CORS_OPTIONS),
+    bodyParser.urlencoded({ extended: true })
+];
+
+const app  = express();
+
+app.use(middlewares);
+
+app.get('/',(req,res) => {
+    res.json({message: 'Welcome to The beginning'});
+});
+
+app.listen(CONFIG.SERVER_PORT,() => {
+    console.log(`The server started on port ${CONFIG.SERVER_PORT}`);
+});
