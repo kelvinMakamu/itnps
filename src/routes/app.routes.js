df45b17fad4af69c68504265a10e1e06eae65b9d
Daniel Njoroge Kinyanjui <dkinyanjui@safaricom.co.ke>
2020-11-03 09:43:33 +0300

    Fixes

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,27 +1,27 @@
-const router   = require("express").Router();
+const router = require("express").Router();
 /********
- * 
+ *
  * CTRLS
  *
-**********/
-const authCtrl   = require('../controllers/auth.controller');
-const userCtrl   = require('../controllers/user.controller');
-const authorized = [ authCtrl.loginRequired, authCtrl.isValidToken ];
+ **********/
+const authCtrl = require("../controllers/auth.controller");
+const userCtrl = require("../controllers/user.controller");
+const authorized = [authCtrl.loginRequired, authCtrl.isValidToken];
 /*****
- * 
+ *
  * AUTH
  *
-********/
+ ********/
 router.post("/auth/login", authCtrl.login);
-router.post("/auth/register",authCtrl.registerUser);
+router.post("/auth/register", authCtrl.registerUser);
 
 /******
- * 
+ *
  * USERS
  *
-********/
-router.get("/users",userCtrl.findUsers);
-router.get("/users/:id",userCtrl.findUserById);
-router.post("/users/userLevel",userCtrl.findUserByLevel);
+ ********/
+router.get("/users", userCtrl.findUsers);
+router.get("/users/:id", userCtrl.findUserById);
+router.post("/users/userLevel", userCtrl.findUserByLevel);
 
-module.exports  = router;
+module.exports = router;
