5d2c6fa20e3c3c06a3385e45e61e5d69be8728ca
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-11-05 03:48:39 +0300

    Designed a frontend design layout for the app

--- itnps-client/src/app/app.module.ts
+++ itnps-client/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TnpsTrendsComponent } from './tnps-trends/tnps-trends.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    DashboardComponent,
+    TnpsTrendsComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
