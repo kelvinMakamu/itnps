bf1e8baaa8bf6698fb1a3fe48e1654af4f590adc
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-11-06 23:29:51 +0300

    Added a second API URL

--- itnps-client/src/environments/environment.ts
+++ itnps-client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  API_URL: `http://localhost:8185/api/v1/`
+  API_URL: `http://localhost:8185/api/v1/`,
+  responsesApiEndpoint: 'http://localhost:8185/api/v1/responses',
 };
 
 /*
